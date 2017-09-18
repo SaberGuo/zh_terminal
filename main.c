6fcafbe3c074a84f08d2a8e02f921a3c38dcaca3
saber <viva_6059@163.com>
2017-09-18 08:22:08 +0800

    add power ctrl

--- main.c
+++ main.c
@@ -12,6 +12,7 @@
 #include "fatfs/ff.h"
 #include "adc/adc.h"
 #include "app/protocol/protocol.h"
+#include "app/power/power.h"
 
 
 #ifdef HAS_VAISAL
@@ -41,31 +42,19 @@ int main(){
     config_relay(RELAY_K_GSM|RELAY_K_CAMERA|RELAY_K_IO|RELAY_K_IO_POWER);
     reset_relay(RELAY_K_GSM|RELAY_K_CAMERA|RELAY_K_IO|RELAY_K_IO_POWER);
     //dcdc disable
-    MAP_GPIO_setAsOutputPin(GPIO_PORT_P5, GPIO_PIN6);
-    MAP_GPIO_setOutputHighOnPin(GPIO_PORT_P5, GPIO_PIN6);
-    MAP_GPIO_setAsOutputPin(GPIO_PORT_P3, GPIO_PIN0);
-    MAP_GPIO_setOutputHighOnPin(GPIO_PORT_P3, GPIO_PIN0);
-    MAP_GPIO_setAsOutputPin(GPIO_PORT_P10, GPIO_PIN2);
-        MAP_GPIO_setOutputHighOnPin(GPIO_PORT_P10, GPIO_PIN2);
-    //delay_ms(1000);
+    power_manage_init();
+    dc_5v_disable();
+    dc_33v_disable();
+    tfcard_disable();
+
     //clock init
     clock_init();
-    /*power on*/
-    //set_relay(RELAY_K_IO|RELAY_K_CAMERA);
-    //camera_init();
-
-    //tfcard init
-    //f_mount(&fatfs, "",0);
-
+    //adc init
     adc14_init();
     //timer init
     timer_init();
 
-#ifdef HAS_VAISAL
-    //init for sensor vaisal
-    vaisal_init();
-    vaisal_close();
-#endif
+
 
 
 #ifdef HAS_TH10S
@@ -105,11 +94,27 @@ void main_timer_process(){
     uint8_t i = 0;
     //open 485 serial port and IO 12V
     set_relay(RELAY_K_IO|RELAY_K_IO_POWER);
+    dc_5v_enable();
+    dc_33v_enable();
+
+    //adc sample
+    adc14_start_sample();
+    delay_ms(100);
+    /*
+     * for(i =0;i<8;++i)
+     * printf("adc14 channel %d is %f\n", i, get_adc14_value(i));
+     */
+    main_timer_irq_flag = false;
+    adc14_stop_sample();
+
 #ifdef HAS_VAISAL
+
+    vaisal_init();
     delay_ms(1000); //wait for 10s with
     start_vaisal_rev();
     delay_ms(10000);
     stop_vaisal_rev();
+    vaisal_close();
 
 #endif
     reset_relay(RELAY_K_IO|RELAY_K_IO_POWER);
@@ -122,7 +127,6 @@ void main_timer_process(){
 
     //send msg
     ret=upload_data_start();
-
 #ifdef HAS_VAISAL
     if(ret){
         for(i = 0;i<TH_DATAS_NUM;++i){
@@ -131,6 +135,7 @@ void main_timer_process(){
     }
 #endif
     ret&=upload_data_end();
+
 
     if(ret == false){//send fail
 
